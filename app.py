91ef8003866cdc6d7857a3d78c0c744abd809396
S2502968 <S2502968@ndonline.ac.uk>
2026-06-15 10:45:53 +0100

    created a basic login and register and home page

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home_page():
+    return render_template("home.html")
+
+@app.route('/login')
+def login_page():
+    return render_template("login.html")
+
+@app.route('/register')
+def register_page():
+    return render_template("register.html")
+
 
 
 if __name__ == '__main__':
